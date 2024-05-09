237a81d2b62127dc53a862dc0fb538f05fb1cc37
Kostja P <kpavlovsky@users.noreply.github.com>
2024-05-09 17:26:26 +0200

    Initial commit

---
+++ myblog/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-r&2nm5zz*=t3ba0l!$!lkje5ciazrrm3n3x#)rwte&ii9)@z7x"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
